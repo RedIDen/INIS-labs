6ecbaa5899753e535dbdee6b850c6aaa8770253b
denios.vasilios@gmail.com <denios.vasilios@gmail.com>
2022-03-18 18:37:08 +0300

    Implement last page (details.html). Minor design fixes.

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-// NOTE: The variable "shirts" is defined in the shirts.js file as the full list of shirt offerings
-//       You can access this variable here, and should use this variable here to build your webpages
-
 let initProducts = () => {
     const container = document.querySelector('.imageContainer');
-    shirts.forEach(x => {
+    for (let i = 0; i < shirts.length; i++) {
+        let x = shirts[i];
         let div = document.createElement('div');
         div.classList.add('tshirtContainer');
 
@@ -20,7 +18,8 @@ let initProducts = () => {
 
         let p = document.createElement('p');
         p.classList.add("pItem");
-        let coutOfColors = countColors(x);
+        localStorage.setItem('shirtIndex', i);
+        let coutOfColors = countColors();
         let many = coutOfColors == 1 ? '' : 's';
         p.innerHTML = `Available in ${coutOfColors} color${many}`
         div.append(p);
@@ -32,7 +31,8 @@ let initProducts = () => {
         buttonQuickView.innerHTML = 'Quick View';
         buttonQuickView.classList.add('buttonItem');
         buttonQuickView.addEventListener('click', () => {
-            showQuickView(x);
+            localStorage.setItem('shirtIndex', i);
+            showQuickView();
         });
         buttons.append(buttonQuickView);
 
@@ -40,50 +40,119 @@ let initProducts = () => {
         buttonSeePage.innerHTML = 'See Page';
         buttonSeePage.classList.add('buttonItem');
         buttonSeePage.addEventListener('click', () => {
-            initDetails(x);
+            window.location = '/details.html';
+            localStorage.setItem('shirtIndex', i);
         });
         buttons.append(buttonSeePage);
 
         div.append(buttons);
 
         container.append(div);
-    });
+    }
 };
 
-let countColors = (x) => {
+let countColors = () => {
+    let shirt = shirts[localStorage.getItem('shirtIndex')];
     let result = 0;
-    for (let e in x.colors) {
-
+    for (let e in shirt.colors) {
         result++;
     }
 
     return result;
 };
 
-let initDetails = (x) => {
-    window.location = 'details.html';
+let initDetails = () => {
+    let side = 'front';
+    let activeColor = 'white';
+    let shirtName = document.querySelector('.shirtTitle');
+    let detailImage = document.querySelector('.shirtImage');
+    let shirtInfo = document.querySelector('.shirtInfo');
+    let shirtPrice = document.querySelector('.shirtPrice');
+    let shirtDescription = document.querySelector('.shirtDescription');
+    let frontSideBtn = document.querySelector('.frontSideButton');
+    let backSideBtn = document.querySelector('.backSideButton');
+    let shirtColors = document.querySelector('.shirtColors');
+
+    let shirt = shirts[localStorage.getItem('shirtIndex')];
+    const image = new Image();
+    shirtName.textContent = shirt.name;
+
+    image.src = `${shirt.colors[Object.keys(shirt.colors)[0]].front}`;
+    detailImage.style.backgroundImage = '';
+    detailImage.append(image);
+
+    shirtDescription.textContent = `${shirt.description}`
+    shirtInfo.prepend(shirtDescription);
+
+    shirtPrice.textContent = `${shirt.price}`;
+    shirtInfo.prepend(shirtPrice);
+
+    for (let i = 0; i < Object.keys(shirt.colors).length; i++) {
+        let colorBtn = document.createElement('button');
+        colorBtn.classList.add('colorButton');
+        if (i == 0) {
+            colorBtn.classList.add('active');
+        }
+        colorBtn.textContent = Object.keys(shirt.colors)[i];
+        colorBtn.style.backgroundColor = Object.keys(shirt.colors)[i];
+        shirtColors.append(colorBtn);
+    }
+    const colorBtns = document.querySelectorAll('.colorButton');
+    frontSideBtn.addEventListener('click', e => {
+        backSideBtn.classList.remove('active');
+        frontSideBtn.classList.add('active');
+        swipeSide(shirt, 'front');
+        side = 'front';
+    });
+    backSideBtn.addEventListener('click', e => {
+        frontSideBtn.classList.remove('active');
+        backSideBtn.classList.add('active');
+        swipeSide(shirt, 'back');
+        side = 'back';
+    });
+    shirtColors.addEventListener('click', e => {
+        if (e.target.className == 'colorButton') {
+            colorBtns.forEach((element) => {
+                if (element.classList.contains('active')) {
+                    element.classList.remove('active');
+                }
+            })
+            e.target.classList.add('active');
+            activeColor = e.target.textContent;
+            swipeSide(shirt, side);
+        }
+        else {
+            return;
+        }
+    });
+    function swipeSide(shirt, side) {
+        image.src = `${shirt.colors[activeColor][side]}`;
+        detailImage.style.backgroundImage = '';
+        detailImage.append(image);
+    }
 };
 
 let navClick = (link) => {
     window.location = link;
 };
 
-let showQuickView = (x) => {
+let showQuickView = () => {
+    let shirt = shirts[localStorage.getItem('shirtIndex')];
     const container = document.querySelector('.quickViewContainer');
-
+    console.log(shirt);
     clearQuickView(container);
 
     let imageContent = document.createElement('div');
 
     let frontImg = document.createElement('img');
     frontImg.classList.add('imgQuickView');
-    frontImg.src = `${x.colors.white.front}`;
+    frontImg.src = `${shirt.colors.white.front}`;
     frontImg.alt = "Front View";
     imageContent.append(frontImg);
 
     let backImg = document.createElement('img');
     backImg.classList.add('imgQuickView');
-    backImg.src = `${x.colors.white.back}`;
+    backImg.src = `${shirt.colors.white.back}`;
     backImg.alt = "Back View";
     imageContent.append(backImg);
 
@@ -93,11 +162,11 @@ let showQuickView = (x) => {
 
     let tShirtName = document.createElement('p');
     tShirtName.classList.add('name');
-    tShirtName.innerHTML = `${x.name}`;
+    tShirtName.innerHTML = `${shirt.name}`;
 
     let price = document.createElement('p');
     price.classList.add('price');
-    price.innerHTML = `${x.price}`;
+    price.innerHTML = `${shirt.price}`;
 
     let button = document.createElement('button');
     button.innerHTML = 'Close';
