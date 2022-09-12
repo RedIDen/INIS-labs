e7e9dc86c158e3cf7209038a5417b05ded06a61b
denios.vasilios@gmail.com <denios.vasilios@gmail.com>
2022-09-12 20:45:47 +0300

    Make laba2 7 sem

--- INIS-lab2/input.js
+++ INIS-lab2/input.js
@@ -1,230 +1,89 @@
-const minWidth = 50;
-const minHeight = 50;
+const shapeButtons = document.querySelectorAll('.shapeBtn');
+const clearButton = document.querySelector('.clearBtn');
+const menu = document.querySelector('.menu');
+const svg = document.querySelector('svg');
 
-const targets = document.querySelectorAll('.target');
+const svgns = "http://www.w3.org/2000/svg";
 
-let selected = null;
-let captured = null;
-let resized = null;
+var drawing;
+var indexOfShape = 0;
+var selectedButton = document.querySelector('.selectedBtn');
+let shape;
 
-let moved = false;
-let escaped = false;
-let dbclicked = false;
-let dbtapped = false;
-let mouse = true;
+var x;
+var y;
 
-let deltaX;
-let deltaY;
+// Shape buttons
+shapeButtons.forEach(x => x.addEventListener('click', () => {
+    if (selectedButton != x) {
+        selectedButton.classList.remove('selectedBtn');
+        x.classList.add('selectedBtn');
+        selectedButton = x;
+        indexOfShape = Array.prototype.indexOf.call(shapeButtons, x);
+        //  console.log(indexOfShape);
+    }
+}));
 
-let startX;
-let startY;
+// Clear button
+clearButton.addEventListener('click', () => {
+    let child = svg.lastElementChild;
+    while (child) {
+        svg.removeChild(child);
+        child = svg.lastElementChild;
+    }
+});
 
-let startWidth;
-let startHeight;
+svg.addEventListener('mousedown', (e) => {
+    drawing = true;
+    x = e.clientX;
+    y = e.clientY;
+    switch (indexOfShape) {
+        case 0: // circle
+            shape = document.createElementNS(svgns, "circle");
+            shape.setAttribute("cx", e.clientX - menu.clientWidth);
+            shape.setAttribute("cy", e.clientY);
+            shape.setAttribute("fill", "red");
+            svg.appendChild(shape);
+            break;
+        case 1: // rectangle
+            shape = document.createElementNS(svgns, "rect");
+            shape.setAttribute("x", e.clientX - menu.clientWidth);
+            shape.setAttribute("y", e.clientY);
+            shape.setAttribute("fill", "red");
+            svg.appendChild(shape);
+            break;
+    }
+})
 
-let tapTimer = null;
-
-function subscribe() {
-
-    // clicks --------------------------------------------
-
-    document.addEventListener('click', () => {
-        touchBackgroung();
-    });
-
-    document.addEventListener('mousemove', (e) => {
-        if (mouse) {
-            moveElement(e);
-        }
-    });
-
-    document.addEventListener('mouseup', () => {
-        dbclicked = false;
-        captured = null;
-    });
-
-    document.addEventListener('keydown', (e) => {
-        if (e.key == 'Escape') {
-            cancel();
-            escaped = !dbclicked;
-        }
-    });
-
-    // touches -----------------------------------------------
-
-    document.addEventListener('touchmove', (e) => {
-        if (!dbtapped) {
-            moveElement(e.touches[0]);
-        }
-        else {
-            resize(e);
-        }
-    });
-
-    document.addEventListener('touchend', () => {
-        if (!moved) {
-            captured = null;
-        }
-    });
-
-    document.addEventListener('touchstart', (e) => {
-        mouse = false;
-        switch (e.touches.length) {
-            case 2:
-                cancel();
+svg.addEventListener('mousemove', (e) => {
+    if (drawing) {
+        switch (indexOfShape) {
+            case 0: // circle
+                let radius = Math.max(e.clientX - x, e.clientY - y) / 2;
+                shape.setAttribute("cx", x + radius - menu.clientWidth);
+                shape.setAttribute("cy", y + radius);
+                shape.setAttribute("r", radius);
                 break;
-            case 3:
-                resized.style.width = startWidth + 'px';
-                resized.style.height = startHeight + 'px';
-                resized.style.left = startX + 'px';
-                resized.style.top = startY + 'px';
-                resized = null;
+            case 1: // rectangle
+                if (e.clientX > x) {
+                    shape.setAttribute("width", e.clientX - x);
+                } else {
+                    shape.setAttribute("x", e.clientX - menu.clientWidth);
+                    shape.setAttribute("width", x - e.clientX);
+                }
+
+                if (e.clientY > y) {
+                    shape.setAttribute("height", e.clientY - y);
+                } else {
+                    shape.setAttribute("y", e.clientY);
+                    shape.setAttribute("height", y - e.clientY);
+                }
+
                 break;
         }
-    });
-
-    targets.forEach(element => {
-
-        // clicks -------------------------------------------------
-
-        element.addEventListener('click', (e) => {
-            touchElement(element, e);
-        });
-
-        element.addEventListener('mousedown', (e) => {
-            downElement(element, e);
-        });
-
-        element.addEventListener('dblclick', (e) => {
-            dbclicked = true;
-            captureElement(element, e);
-        });
-
-        // touches --------------------------------------------------
-
-        element.addEventListener('touchstart', (e) => {
-            mouse = false;
-            switch (e.touches.length) {
-                case 1:
-                    if (tapTimer == null) {
-                        tapTimer = setTimeout(function () {
-                            tapTimer = null;
-                        }, 200)
-
-                        dbtapped = false;
-                        moved = false;
-                        if (captured == null) {
-                            downElement(element, e.touches[0]);
-                        }
-                    }
-                    else {
-                        clearTimeout(tapTimer);
-                        tapTimer = null;
-                        captureElement(element, e.touches[0]);
-                        dbclicked = true;
-                    }
-                    break;
-                case 2:
-                    if (tapTimer != null) {
-                        clearTimeout(tapTimer);
-                        tapTimer = null;
-                        dbtapped = true;
-                        resized = element;
-                        startX = element.offsetLeft;
-                        startY = element.offsetTop;
-                        startWidth = element.offsetWidth;
-                        startHeight = element.offsetHeight;
-                        e.stopPropagation();
-                    }
-                    break;
-            }
-        });
-
-        element.addEventListener('touchend', (e) => {
-            mouse = false;
-            switch (e.touches.length) {
-                case 0:
-                    if (captured != null && !dbclicked) {
-                        captured = null;
-                    }
-
-                    if (!dbtapped) {
-                        touchElement(element, e);
-                    }
-
-                    resized = null;
-                    break;
-            }
-        });
-
-    });
-}
-
-subscribe();
-
-function captureElement(element, e) {
-    startX = element.offsetLeft;
-    startY = element.offsetTop;
-    deltaX = element.offsetLeft - e.clientX;
-    deltaY = element.offsetTop - e.clientY;
-    captured = element;
-}
-
-function touchElement(element, e) {
-    if (!moved) {
-        if (selected != null) {
-            selected.classList.remove('selected');
-        }
-
-        selected = element;
-        selected.classList.add('selected');
     }
+})
 
-    e.stopPropagation();
-}
-
-function touchBackgroung() {
-    if (!escaped) {
-        if (selected != null) {
-            selected.classList.remove('selected');
-            selected = null;
-        }
-    }
-
-    escaped = false;
-}
-
-function downElement(element, e) {
-    if (captured == null) {
-        captureElement(element, e);
-        moved = false;
-        dbclicked = false;
-    }
-}
-
-function moveElement(e) {
-    if (captured != null) {
-        captured.style.left = (e.clientX + deltaX) + 'px';
-        captured.style.top = (e.clientY + deltaY) + 'px';
-        moved = true;
-    }
-}
-
-function cancel() {
-    if (captured != null) {
-        captured.style.left = startX + 'px';
-        captured.style.top = startY + 'px';
-        captured = null;
-    }
-}
-
-function resize(e) {
-    let width = Math.abs(e.touches[0].clientX - e.touches[1].clientX);
-    let height = Math.abs(e.touches[0].clientY - e.touches[1].clientY);
-
-    resized.style.width = Math.max(width, minWidth) + 'px';
-    resized.style.height = Math.max(height, minHeight) + 'px';
-
-    resized.style.left = Math.min(e.touches[0].clientX, e.touches[1].clientX) + 'px';
-    resized.style.top = Math.min(e.touches[0].clientY, e.touches[1].clientY) + 'px';
-}
+svg.addEventListener('mouseup', () => {
+    drawing = false;
+})
